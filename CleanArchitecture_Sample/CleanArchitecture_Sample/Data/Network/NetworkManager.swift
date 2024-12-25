e321e3d25858fc79e272786358c4609f22616a99
Qussk <by1106007@gmail.com>
2024-12-25 21:50:19 +0900

    feat: cell UI작업 (Fix: 디코딩 버그수정)

--- CleanArchitecture_Sample/CleanArchitecture_Sample/Data/Network/NetworkManager.swift
+++ CleanArchitecture_Sample/CleanArchitecture_Sample/Data/Network/NetworkManager.swift
@@ -20,26 +20,19 @@ public class NetworkManager: NetworkManagerProtocol {
     
 
     private let tokenHeader: HTTPHeaders = {
-        let tokenHeader = HTTPHeader(name: "Authorization", value: "Bearer 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")
+        let tokenHeader = HTTPHeader(name: "Authorization", value: "Bearer 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")
         return HTTPHeaders([tokenHeader])
     }()
-    
-//    var httpHeaders: [String: String] {
-//        var headers: [String: String] = [:]
-//        let accessToken = "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"
-//        headers["Authorization"] = "Bearer \(accessToken)"
-//        return headers
-//    }
-
     
     func fetchData<T: Decodable>(url: String, method: HTTPMethod, paramerers: Parameters?) async -> Result<T, NetworkError> {
         guard let url = URL(string: url) else { return .failure(.urlError) }
         
-        let result = await session.request(url, method: method, paramerers: paramerers!, headers: tokenHeader).serializingData().response
+        let result = await session.request(url, method: method, paramerers: paramerers, headers: tokenHeader).serializingData().response
         if let error = result.error { return .failure(.requestFailed(error.localizedDescription)) }
         guard let data = result.data else { return .failure(.dataNil) }
         guard let response = result.response else { return .failure(.invalid)}
-        if 200 ..< 300 ~= response.statusCode {
+    
+        if 200..<400 ~= response.statusCode {
             do {
                 let data = try JSONDecoder().decode(T.self, from: data)
                 return .success(data)
--- CleanArchitecture_Sample/CleanArchitecture_Sample/Presentation/ViewController/UserListViewController.swift
+++ CleanArchitecture_Sample/CleanArchitecture_Sample/Presentation/ViewController/UserListViewController.swift
@@ -34,6 +34,7 @@ class UserListViewController: UIViewController {
         
     private let tableView = {
         let tableView = UITableView()
+        tableView.register(UserTableViewCell.self, forCellReuseIdentifier: "UserTableViewCell")
         return tableView
     }()
     
@@ -55,15 +56,18 @@ class UserListViewController: UIViewController {
         let tabbuttonType = tabButtonView.selectedType.compactMap { $0 } //옵셔널제거
         let query = searchTextField.rx.text.orEmpty.debounce(.milliseconds(300), scheduler: MainScheduler.instance)
         let outPut = viewModel.trenform(input: UserListViewModel.Input(tabButtonType: tabbuttonType, query: query, saveFavorite: saveFavorit.asObservable(), deleteFavorite: deleteFavoriy.asObservable(), fetchMore: fetchMore.asObservable()))
-        
+                
         outPut.cellData.bind(to: tableView.rx.items) { tableView, index, item in
-            return UITableViewCell()
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "UserTableViewCell") else { return UITableViewCell() }
+            (cell as? UserTableViewCell)?.apply(cellData: item)
+            return cell
         }.disposed(by: disposeBag)
         
         outPut.error.bind { [weak self] ms in
-            let alert = UIAlertController(title: "알림", message: ms, preferredStyle: .alert)
-            alert.addAction(.init(title: "확인", style: .default))
-            self?.present(alert, animated: true)
+            print(ms)
+//            let alert = UIAlertController(title: "알림", message: ms, preferredStyle: .alert)
+//            alert.addAction(.init(title: "확인", style: .default))
+//            self?.present(alert, animated: true)
         }.disposed(by: disposeBag)
     }
     
@@ -99,76 +103,3 @@ class UserListViewController: UIViewController {
     }
 }
 
-final class TabButtonView: UIStackView {
-    private let tabList: [TabButtonType]
-    private let disposeBag = DisposeBag()
-    public let selectedType: BehaviorRelay<TabButtonType?>
-    init(tabList: [TabButtonType]) {
-        self.tabList = tabList
-        self.selectedType = BehaviorRelay(value: tabList.first)
-        super.init(frame: .zero)
-        alignment = .fill
-        distribution = .fillEqually
-        
-        addButton()
-        (arrangedSubviews.first as? UIButton)?.isSelected = true //버튼 초깃값
-    }
-    
-    private func addButton() {
-        tabList.forEach { type in
-            let button = TabButton(type: type)
-            button.rx.tap.bind { [weak self] in
-                self?.arrangedSubviews.forEach { view in
-                    (view as? UIButton)?.isSelected = false //나머지 모두 false
-                }
-                button.isSelected = true
-                self?.selectedType.accept(type)
-            }.disposed(by: disposeBag)
-            addArrangedSubview(button)
-        }
-    }
-    required init(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
-final class TabButton: UIButton {
-    private let type: TabButtonType
-    
-    override var isSelected: Bool {
-        didSet {
-            backgroundColor = isSelected ? .systemOrange : .systemGray5
-        }
-    }
-    init(type: TabButtonType) {
-        self.type = type
-        super.init(frame: .zero)
-        backgroundColor = .systemGray5
-        setTitle(type.title, for: .normal)
-        titleLabel?.font = .systemFont(ofSize: 16, weight: .regular)
-        setTitleColor(.black, for: .normal)
-        setTitleColor(.white, for: .selected)
-        switch type {
-        case .api:
-            applyCorners(corners: [.topLeft, .bottomLeft], radius: 15)
-        case .favorite:
-            applyCorners(corners: [.topRight, .bottomRight], radius: 15)
-        }
-    }
-    required init(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func applyCorners(corners: UIRectCorner, radius: CGFloat) {
-        DispatchQueue.main.async {
-            let path = UIBezierPath(
-                roundedRect: self.bounds,
-                byRoundingCorners: corners,
-                cornerRadii: CGSize(width: radius, height: radius)
-            )
-            let mask = CAShapeLayer()
-            mask.path = path.cgPath
-            self.layer.mask = mask
-        }
-    }
-}
